431b39fa6ead686ebdbf200a6f3caaa6cf01d932
ttyS3 <ttys3.rust@gmail.com>
2022-09-27 03:25:47 +0800

    fixup! chore: init commit

--- src/main.rs
+++ src/main.rs
@@ -10,58 +10,27 @@ use opentelemetry::{Context, global};
 use opentelemetry::metrics::{Counter, Histogram};
 use opentelemetry::sdk::export::metrics::aggregation;
 use opentelemetry::sdk::metrics::{controllers, processors, selectors};
+use crate::middleware::metrics::{PromMetrics, track_metrics};
 
 #[derive(Clone)]
 pub struct SharedState {
     pub root_dir: String,
-    pub metric: Metric,
-}
-
-
-#[derive(Clone)]
-pub struct Metric {
-    pub cx: Context,
-    pub http_counter: Counter<u64>,
-
-    // migrate from ValueRecorder to Histogram if opentelemetry 0.18.0 released
-    pub http_req_histogram: Histogram<f64>,
 }
 
 
 #[tokio::main]
 async fn main() {
-    let meter = global::meter("my-app");
-
     let state = SharedState {
         root_dir: String::from("/tmp"),
-        metric: Metric {
-            cx: Default::default(),
-            http_counter: meter.u64_counter("http.counter")
-                .with_description("Counts http request")
-                .init(),
-            http_req_histogram:  meter.f64_histogram("http.histogram")
-                .with_description("Counts http request latency")
-                .init()
-        }
     };
 
-    // init global meter provider and prometheus exporter
-    let controller = controllers::basic(
-        processors::factory(
-            selectors::simple::histogram([0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1.0, 2.5, 5.0, 10.0]),
-            aggregation::cumulative_temporality_selector(),
-        )
-            .with_memory(true),
-    )
-        .build();
-    let exporter = opentelemetry_prometheus::exporter(controller).
-        init();
+    let metrics = PromMetrics::new();
 
     // build our application with a route
     let app = Router::with_state(state.clone())
-        .merge(crate::middleware::metrics::routes(exporter))
+        .merge(metrics.routes())
         .route("/", get(handler))
-        .route_layer(axum::middleware::from_fn_with_state(state.clone(), middleware::metrics::track_metrics));
+        .route_layer(axum::middleware::from_fn_with_state(metrics.state.clone(), track_metrics));
 
     // run it
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
--- src/middleware/metrics.rs
+++ src/middleware/metrics.rs
@@ -1,10 +1,14 @@
 use axum::{extract::MatchedPath, http::Request, response::IntoResponse, routing::get, Router};
-use axum::middleware::{Next};
+use axum::middleware::{FromFnLayer, Next};
 use std::{
     time::{Instant},
 };
-use std::future::ready;
+use std::convert::Infallible;
+use std::future::{Future, ready};
 use axum::extract::State;
+use axum::http::Response;
+use axum::routing::Route;
+use axum_core::Error;
 
 use opentelemetry_prometheus::PrometheusExporter;
 
@@ -15,19 +19,78 @@ use opentelemetry::{
 };
 use axum_macros::debug_handler;
 
-#[derive(Clone)]
-pub struct AppState {
-    exporter: PrometheusExporter,
+use opentelemetry::{Context, global};
+use opentelemetry::metrics::{Counter, Histogram};
+use opentelemetry::sdk::export::metrics::aggregation;
+use opentelemetry::sdk::metrics::{controllers, processors, selectors};
+
+pub struct PromMetrics {
+    pub(crate) state: MetricState,
 }
 
-pub fn routes(exporter: PrometheusExporter) -> Router<AppState> {
-    let app_state = AppState { exporter };
-    Router::with_state(app_state)
-        .route("/metrics", get(exporter_handler))
+#[derive(Clone)]
+pub struct MetricState {
+    exporter: PrometheusExporter,
+    pub metric: Metric,
+}
+
+#[derive(Clone)]
+pub struct Metric {
+    pub cx: Context,
+    pub http_counter: Counter<u64>,
+
+    // migrate from ValueRecorder to Histogram if opentelemetry 0.18.0 released
+    pub http_req_histogram: Histogram<f64>,
+}
+
+impl PromMetrics
+{
+    pub fn new() -> Self {
+
+        let meter = global::meter("my-app");
+
+        // init global meter provider and prometheus exporter
+        let controller = controllers::basic(
+            processors::factory(
+                selectors::simple::histogram([0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1.0, 2.5, 5.0, 10.0]),
+                aggregation::cumulative_temporality_selector(),
+            )
+                .with_memory(true),
+        )
+            .build();
+        let exporter = opentelemetry_prometheus::exporter(controller).
+            init();
+
+        let app_state = MetricState {
+            exporter,
+            metric: Metric {
+            cx: Default::default(),
+            http_counter: meter.u64_counter("http.counter")
+                .with_description("Counts http request")
+                .init(),
+            http_req_histogram:  meter.f64_histogram("http.histogram")
+                .with_description("Counts http request latency")
+                .init()
+        } };
+
+        Self { state: app_state }
+    }
+
+
+    pub fn routes(&self) -> Router<MetricState> {
+        Router::with_state(self.state.clone())
+            .route("/metrics", get(exporter_handler))
+    }
+
+    // fn track_middleware<L>(self) -> FromFnLayer<F, S, T>
+    // {
+    //     axum::middleware::from_fn_with_state(self.state.clone(), track_metrics)
+    // }
 }
 
 #[debug_handler]
-pub async fn exporter_handler(state: State<AppState>) -> impl IntoResponse {
+pub async fn exporter_handler(state: State<MetricState>) -> impl IntoResponse {
+    println!("metrics api");
     let mut buffer = Vec::new();
     let encoder = TextEncoder::new();
     encoder.encode(&state.exporter.registry().gather(), &mut buffer).unwrap();
@@ -35,8 +98,9 @@ pub async fn exporter_handler(state: State<AppState>) -> impl IntoResponse {
     metrics
 }
 
-// warning: the state is not an extractor!
-pub async fn track_metrics<B>(state: State<crate::SharedState>, req: Request<B>, next: Next<B>) -> impl IntoResponse {
+
+// record handler metrics
+pub async fn track_metrics<B>(state: State<MetricState>, req: Request<B>, next: Next<B>) -> axum::response::Response {
     let start = Instant::now();
     let path = if let Some(matched_path) = req.extensions().get::<MatchedPath>() {
         matched_path.as_str().to_owned()
@@ -64,6 +128,9 @@ pub async fn track_metrics<B>(state: State<crate::SharedState>, req: Request<B>,
     state.metric.http_counter.add(&state.metric.cx, 1, &labels);
 
     state.metric.http_req_histogram.record(&state.metric.cx, latency, &labels);
+
+    tracing::info!("{}", method);
+    println!("ok");
 
     response
 }
