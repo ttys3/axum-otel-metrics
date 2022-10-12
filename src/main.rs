e71a421c8b033bff00f8ccdf73fd2a8d6ef9f269
ttyS3 <ttys3.rust@gmail.com>
2022-10-13 04:25:19 +0800

    chore: impl layer and service

--- src/main.rs
+++ src/main.rs
@@ -10,27 +10,37 @@ use opentelemetry::{Context, global};
 use opentelemetry::metrics::{Counter, Histogram};
 use opentelemetry::sdk::export::metrics::aggregation;
 use opentelemetry::sdk::metrics::{controllers, processors, selectors};
-use crate::middleware::metrics::{PromMetrics, track_metrics};
+use tower::Layer;
+use crate::middleware::metrics::{PromMetrics, PromMetricsLayer};
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 #[derive(Clone)]
 pub struct SharedState {
     pub root_dir: String,
 }
 
-
 #[tokio::main]
 async fn main() {
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::EnvFilter::new(
+            std::env::var("RUST_LOG")
+                .unwrap_or_else(|_| "axum_metrics_demo=debug,tower_http=info".into()),
+        ))
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
     let state = SharedState {
         root_dir: String::from("/tmp"),
     };
 
-    let metrics = PromMetrics::new();
+    let metrics = PromMetricsLayer::new();
 
     // build our application with a route
     let app = Router::with_state(state.clone())
         .merge(metrics.routes())
         .route("/", get(handler))
-        .route_layer(axum::middleware::from_fn_with_state(metrics.state.clone(), track_metrics));
+        .route("/hello", get(handler))
+        .route_layer(metrics);
 
     // run it
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
--- src/middleware/metrics.rs
+++ src/middleware/metrics.rs
@@ -1,13 +1,19 @@
-use axum::{extract::MatchedPath, http::Request, response::IntoResponse, routing::get, Router};
+#![feature(generic_associated_types)]
+#![feature(type_alias_impl_trait)]
+
+use axum::{extract::MatchedPath, http::Request, response::IntoResponse, routing::get, Router, http};
 use axum::middleware::{FromFnLayer, Next};
-use std::{
-    time::{Instant},
-};
+use std::{fmt, time::{Instant}};
 use std::convert::Infallible;
 use std::future::{Future, ready};
+use std::pin::Pin;
+use std::sync::Arc;
+use std::task::{Context, Poll};
+use std::task::Poll::Ready;
 use axum::extract::State;
 use axum::http::Response;
 use axum::routing::Route;
+use axum_core::body::BoxBody;
 use axum_core::Error;
 
 use opentelemetry_prometheus::PrometheusExporter;
@@ -19,13 +25,26 @@ use opentelemetry::{
 };
 use axum_macros::debug_handler;
 
-use opentelemetry::{Context, global};
+use opentelemetry::{Context as OtelContext, global};
 use opentelemetry::metrics::{Counter, Histogram};
 use opentelemetry::sdk::export::metrics::aggregation;
 use opentelemetry::sdk::metrics::{controllers, processors, selectors};
 
-pub struct PromMetrics {
-    pub(crate) state: MetricState,
+use tower::{Layer, Service};
+use tower::layer::{layer_fn, LayerFn};
+
+use futures_util::future::BoxFuture;
+use futures_util::ready;
+use pin_project_lite::pin_project;
+
+
+#[derive(Clone)]
+pub struct Metric {
+    pub cx: OtelContext,
+    pub http_counter: Counter<u64>,
+
+    // migrate from ValueRecorder to Histogram if opentelemetry 0.18.0 released
+    pub http_req_histogram: Histogram<f64>,
 }
 
 #[derive(Clone)]
@@ -35,17 +54,25 @@ pub struct MetricState {
 }
 
 #[derive(Clone)]
-pub struct Metric {
-    pub cx: Context,
-    pub http_counter: Counter<u64>,
-
-    // migrate from ValueRecorder to Histogram if opentelemetry 0.18.0 released
-    pub http_req_histogram: Histogram<f64>,
+pub struct PromMetrics<S> {
+    pub(crate) state: Arc<MetricState>,
+    service: S,
 }
 
-impl PromMetrics
-{
+#[derive(Clone)]
+pub struct PromMetricsLayer {
+    pub(crate) state: Arc<MetricState>,
+}
+
+impl PromMetricsLayer {
+
     pub fn new() -> Self {
+        Self {
+            state: Arc::new(Self::new_state()),
+        }
+    }
+
+    pub fn new_state() -> MetricState {
 
         let meter = global::meter("my-app");
 
@@ -64,73 +91,125 @@ impl PromMetrics
         let app_state = MetricState {
             exporter,
             metric: Metric {
-            cx: Default::default(),
-            http_counter: meter.u64_counter("http.counter")
-                .with_description("Counts http request")
-                .init(),
-            http_req_histogram:  meter.f64_histogram("http.histogram")
-                .with_description("Counts http request latency")
-                .init()
-        } };
+                cx: Default::default(),
+                http_counter: meter.u64_counter("http.counter")
+                    .with_description("Counts http request")
+                    .init(),
+                http_req_histogram:  meter.f64_histogram("http.histogram")
+                    .with_description("Counts http request latency")
+                    .init()
+            } };
 
-        Self { state: app_state }
+        app_state
     }
 
-
-    pub fn routes(&self) -> Router<MetricState> {
+    pub fn routes(&self) -> Router<Arc<MetricState>> {
         Router::with_state(self.state.clone())
             .route("/metrics", get(exporter_handler))
     }
-
-    // fn track_middleware<L>(self) -> FromFnLayer<F, S, T>
-    // {
-    //     axum::middleware::from_fn_with_state(self.state.clone(), track_metrics)
-    // }
 }
 
+impl<S> Layer<S> for PromMetricsLayer {
+    type Service = PromMetrics<S>;
+
+    fn layer(&self, service: S) -> Self::Service {
+        PromMetrics {
+            state: self.state.clone(),
+            service
+        }
+    }
+}
+
+pin_project! {
+    /// Response future for [`PromMetrics`].
+    pub struct ResponseFuture<F> {
+        #[pin]
+        inner: F,
+        #[pin]
+        start: Instant,
+        #[pin]
+        state: Arc<MetricState>,
+        #[pin]
+        path: String,
+        #[pin]
+        method: String,
+    }
+}
+
+impl<S, R, ResBody> Service<Request<R>> for PromMetrics<S>
+    where
+        S: Service<Request<R>, Response = Response<ResBody>>,
+{
+    type Response = Response<ResBody>;
+    type Error = S::Error;
+    type Future = ResponseFuture<S::Future>;
+
+    fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        self.service.poll_ready(cx)
+    }
+
+    fn call(&mut self, req: Request<R>) -> Self::Future {
+        // axum::middleware::from_fn_with_state(self.state.clone(), track_metrics)
+
+        let start = Instant::now();
+        let method = req.method().clone().to_string();
+        let path = if let Some(matched_path) = req.extensions().get::<MatchedPath>() {
+            matched_path.as_str().to_owned()
+        } else {
+            req.uri().path().to_owned()
+        };
+
+        ResponseFuture {
+            inner: self.service.call(req),
+            start,
+            method,
+            path,
+            state: self.state.clone()
+        }
+    }
+}
+
+impl<F, B, E> Future for ResponseFuture<F>
+    where
+        F: Future<Output = Result<Response<B>, E>>,
+{
+    type Output = Result<Response<B>, E>;
+
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        let this = self.project();
+        let response = ready!(this.inner.poll(cx))?;
+
+        if this.path.clone() == "/metrics" {
+            return Ready(Ok(response));
+        }
+
+        let latency = this.start.elapsed().as_secs_f64();
+        let status = response.status().as_u16().to_string();
+
+        let labels = [
+            KeyValue{key: Key::from("method"), value: Value::from(this.method.clone()) },
+            KeyValue::new("path", this.path.clone()),
+            KeyValue::new("status", status.clone()),
+        ];
+
+        this.state.metric.http_counter.add(& this.state.metric.cx, 1, &labels);
+
+        this.state.metric.http_req_histogram.record(&this.state.metric.cx, latency, &labels);
+
+        tracing::info!("method={} latency={} status={} labels={:?}", &this.method, &latency, &status, &labels);
+        println!("ResponseFuture::poll");
+
+        Poll::Ready(Ok(response))
+    }
+}
+
+
 #[debug_handler]
-pub async fn exporter_handler(state: State<MetricState>) -> impl IntoResponse {
+pub async fn exporter_handler(state: State<Arc<MetricState>>) -> impl IntoResponse {
     println!("metrics api");
     let mut buffer = Vec::new();
     let encoder = TextEncoder::new();
     encoder.encode(&state.exporter.registry().gather(), &mut buffer).unwrap();
     let metrics = String::from_utf8(buffer).unwrap();
     metrics
-}
-
-
-// record handler metrics
-pub async fn track_metrics<B>(state: State<MetricState>, req: Request<B>, next: Next<B>) -> axum::response::Response {
-    let start = Instant::now();
-    let path = if let Some(matched_path) = req.extensions().get::<MatchedPath>() {
-        matched_path.as_str().to_owned()
-    } else {
-        req.uri().path().to_owned()
-    };
-
-    let method = req.method().clone();
-
-    let response = next.run(req).await;
-
-    if path == "/metrics" {
-        return response;
-    }
-
-    let latency = start.elapsed().as_secs_f64();
-    let status = response.status().as_u16().to_string();
-
-    let labels = [
-        KeyValue{key: Key::from("method"), value: Value::from(method.to_string()) },
-        KeyValue::new("path", path),
-        KeyValue::new("status", status),
-    ];
-
-    state.metric.http_counter.add(&state.metric.cx, 1, &labels);
-
-    state.metric.http_req_histogram.record(&state.metric.cx, latency, &labels);
-
-    tracing::info!("{}", method);
-    println!("ok");
-
-    response
 }
