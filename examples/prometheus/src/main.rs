8e1acbab25a7a28ce5b302a2b18e94931916df83
ttyS3 <ttys3.rust@gmail.com>
2022-10-14 23:37:29 +0800

    chore: use standalone Cargo.toml for examples

--- examples/prometheus/src/main.rs
+++ examples/prometheus/src/main.rs
@@ -42,7 +42,7 @@ async fn main() {
 
     // run it
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-    println!("listening on {}", addr);
+    println!("listening on http://{}", addr);
     axum::Server::bind(&addr).serve(app.into_make_service()).await.unwrap();
 }
 
