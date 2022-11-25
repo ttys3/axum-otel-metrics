ad287ad571d2a8a6ff3b06fc41076b7ba7bc367f
ttyS3 <ttys3.rust@gmail.com>
2022-11-26 00:42:08 +0800

    chore: clean up example code

--- examples/axum-metrics-demo/src/main.rs
+++ examples/axum-metrics-demo/src/main.rs
@@ -32,7 +32,7 @@ async fn main() {
         .with_service_version(env!("CARGO_PKG_VERSION").to_string())
         .with_prefix("axum_metrics_demo".to_string())
         .with_labels(vec![("env".to_string(), "dev".to_string())].into_iter().collect())
-        .with_skipper(PathSkipper::new(|s: _| s.starts_with("/skip")))
+        .with_skipper(PathSkipper::new(|s| s.starts_with("/skip")))
         .build();
 
     // build our application with a route
