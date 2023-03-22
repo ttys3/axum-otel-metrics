3ae7b69f00bd5349f616537d6805e7c24355f13b
ttyS3 <ttys3.rust@gmail.com>
2023-03-23 03:05:48 +0800

    chore: add TODO about buckets

--- src/lib.rs
+++ src/lib.rs
@@ -101,6 +101,7 @@ pub struct HttpMetricsLayer {
     pub(crate) state: MetricState,
 }
 
+// TODO support custom buckets
 const HTTP_REQ_HISTOGRAM_BUCKETS: &[f64] = &[0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1.0, 2.5, 5.0, 10.0];
 
 impl HttpMetricsLayer {
